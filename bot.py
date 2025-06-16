df1659bb2ec702313131f7b38eaf0be42747e769
mekkkwiz <59276758+mekkkwiz@users.noreply.github.com>
2025-06-17 03:38:08 +0700

    add: function to keep bot alive in render

--- bot.py
+++ bot.py
@@ -6,12 +6,15 @@ import asyncio
 
 from cogs.video_embed import VideoEmbed
 from utils.logger import setup_logger
+from utils.keep_alive import keep_alive
 
 # Load environment variables from .env file
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 if not token:
     raise ValueError("DISCORD_TOKEN not found in .env")
+
+keep_alive() # keep bot alive in render
 
 # Setup logger
 logger = setup_logger("bot")
